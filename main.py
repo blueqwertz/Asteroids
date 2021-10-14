3fa53a8e6dc4a97dca18fccff5b44c0da0ab09c4
Tim Limlei <timlimlei@icloud.com>
2021-10-14 23:03:33 +0200

    colission

--- main.py
+++ main.py
@@ -62,15 +62,97 @@ class Asteroids(object):
         velY = self.player.y - y
         dist = math.sqrt((self.player.x - x) ** 2 + (self.player.y - y) ** 2)
         velX, velY = velX / dist, velY / dist
-        self.enemies.append(Enemy(x, y, (velX, velY)))
+        self.enemies.append(Enemy(x, y, (velX, velY)))    
+    
+    def is_inside_polygon(self, points, p) -> bool:
         
+        INT_MAX = 10000
+ 
+        def onSegment(p:tuple, q:tuple, r:tuple) -> bool:
+            
+            if ((q[0] <= max(p[0], r[0])) &
+                (q[0] >= min(p[0], r[0])) &
+                (q[1] <= max(p[1], r[1])) &
+                (q[1] >= min(p[1], r[1]))):
+                return True
+                
+            return False   
+    
+        def orientation(p:tuple, q:tuple, r:tuple) -> int:
+            
+            val = (((q[1] - p[1]) *
+                    (r[0] - q[0])) -
+                ((q[0] - p[0]) *
+                    (r[1] - q[1])))
+                    
+            if val == 0:
+                return 0
+            if val > 0:
+                return 1 
+            else:
+                return 2 
+        
+        def doIntersect(p1, q1, p2, q2):
+            o1 = orientation(p1, q1, p2)
+            o2 = orientation(p1, q1, q2)
+            o3 = orientation(p2, q2, p1)
+            o4 = orientation(p2, q2, q1)
+            
+            if (o1 != o2) and (o3 != o4):
+                return True
+                    
+            if (o1 == 0) and (onSegment(p1, p2, q1)):
+                return True
+            
+            if (o2 == 0) and (onSegment(p1, q2, q1)):
+                return True
+            
+            if (o3 == 0) and (onSegment(p2, p1, q2)):
+                return True
+            
+            if (o4 == 0) and (onSegment(p2, q1, q2)):
+                return True
+        
+            return False
+        
+        n = len(points)
+        
+        if n < 3:
+            return False
+            
+        extreme = (INT_MAX, p[1])
+        count = i = 0
+        
+        while True:
+            next = (i + 1) % n
+            
+            
+            if (doIntersect(points[i],
+                            points[next],
+                            p, extreme)):
+
+                if orientation(points[i], p,
+                            points[next]) == 0:
+                    return onSegment(points[i], p,
+                                    points[next])
+                                    
+                count += 1                
+            i = next
+            if (i == 0):
+                break
+            
+        return (count % 2 == 1)
     
     def frame(self):
         delta = self.clock.get_rawtime() / 1000
         
         for obj in self.projectiles:
             for el in self.enemies:
-                pass
+                shape = el.shape.copy()
+                for i, point in enumerate(shape):
+                    shape[i] = point[0] + el.x, point[1] + el.y
+                if self.is_inside_polygon(points = shape, p = (obj.x, obj.y)):
+                    self.enemies.remove(el)
             obj.update(delta)
             if obj.x < 0 or obj.x > s_width or obj.y < 0 or obj.y > s_height:
                 self.projectiles.remove(obj)
